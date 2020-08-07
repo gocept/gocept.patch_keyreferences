7e1065d581f8f3f0bcb28b7f59755180db89d0c3
Steffen Allner <sa@gocept.com>
2020-08-07 14:39:13 +0200

    Release preparations.

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "License :: OSI Approved :: Zope Public License",
+        "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
